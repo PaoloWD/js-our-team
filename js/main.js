1241e4afecddde980e17f899326017f094f1d8ed
PaoloWD <paolo.lore.93@gmail.com>
2022-11-15 14:08:04 +0100

    complete bonus 1

--- js/main.js
+++ js/main.js
@@ -5,37 +5,37 @@ const team = [
     name: "Wayne",
     surname: "Barnett",
     role: "Founder & CEO",
-    img: "wayne-barnett-founder-ceo.jpg",
+    img: "img/wayne-barnett-founder-ceo.jpg",
   },
   {
     name: "Angela",
     surname: "Caroll",
     role: "Chief Editor",
-    img: "angela-caroll-chief-editor.jpg",
+    img: "img/angela-caroll-chief-editor.jpg",
   },
   {
     name: "Walter",
     surname: "Gordon",
     role: "Office Manager",
-    img: "walter-gordon-office-manager.jpg",
+    img: "img/walter-gordon-office-manager.jpg",
   },
   {
     name: "Angela",
     surname: "Lopez",
     role: "Social Media Manager",
-    img: "angela-lopez-social-media-manager.jpg",
+    img: "img/angela-lopez-social-media-manager.jpg",
   },
   {
     name: "Scott",
     surname: "Estrada",
     role: "Developer",
-    img: "scott-estrada-developer.jpg",
+    img: "img/scott-estrada-developer.jpg",
   },
   {
     name: "Barbara",
     surname: "Ramos",
     role: "Graphic Designer",
-    img: "barbara-ramos-graphic-designer.jpg",
+    img: "img/barbara-ramos-graphic-designer.jpg",
   },
 ];
 console.log(team);
@@ -54,6 +54,7 @@ function createDiv() {
     const roleEl = document.createElement("div");
     roleEl.innerHTML = info.role;
     const imgEl = document.createElement("img");
+    imgEl.src = info.img;
     imgEl.innerHTML = info.img;
     containerEl.append(nameEl);
     containerEl.append(surnameEl);
